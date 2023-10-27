43e5cfebca7162e7cc3fdf4e413eec6974690ebb
nat9285 <psycho.na30@gmail.com>
2023-10-26 21:15:47 -0300

    Validación Formulario OK

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function valFormulario() {
     let nombre = document.getElementById("nombre").value.trim();
 
-    if (nombre === "") {
+    if (nombre === "" || nombre.lenght == 0) {
         alert("No puede dejar vacío el espacio Nombre");
         return false;
     } else {
