68d41412fed2e6b368c7e59813e147ecc0f9c8e5
nat9285 <psycho.na30@gmail.com>
2023-10-28 23:59:27 -0300

    Inicio de API - incompleto

--- app.js
+++ app.js
@@ -10,3 +10,30 @@ function valFormulario() {
 }
 
 const enviarMensaje = () => alert("ATENCIÓN: Esta acción borrará todos los cambos. ¿Confirma?")
+
+const CargarProductos = async () => {
+    try {
+        const respuesta = await fetch("https://fakestoreapi.com/products");
+        if (respuesta.ok) {
+            const data = await respuesta.json();
+            return data;
+        } else {
+            throw new Error('Error al obtener los productos');
+        }
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
+
+CargarProductos()
+    .then(data => {
+        if (data) {
+            console.log(data);
+        } else {
+            console.log('No se pudieron cargar los productos.');
+        }
+    })
+    .catch(error => {
+        console.error('Ocurrió un error:', error);
+    });
